2ec62fee07eab5fe8997a5a40b7d44b8c3ace04e
lsanchez <leonardo.sanchez.jimenez@una.ac.cr>
2022-04-30 21:25:02 -0600

    Wrong data source for RichfacesA4J

--- src/parse-engines/types/richfaces45-a4j-engine.ts
+++ src/parse-engines/types/richfaces45-a4j-engine.ts
@@ -1,13 +1,13 @@
 import IParseEngine from "../common/parse-engine";
 import ComponentExtractor from "../common/component-extractor";
 import ComponentDefinition from "../../common/component-definition";
-import * as primefaces10 from '../data/richfaces45.json';
+import * as data from '../data/richfaces45-a4j.json';
 
 class RichfacesA4JParseEngine implements IParseEngine {
     public languageId = "richfaces45-a4j";
 
     public async parse(version: string): Promise<ComponentDefinition[]> {
-        const textJson = JSON.stringify(primefaces10.components.component);
+        const textJson = JSON.stringify(data.components.component);
         return ComponentExtractor.extract(textJson);
     }
 }
